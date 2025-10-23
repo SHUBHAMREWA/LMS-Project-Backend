23cb7d76cfeb4460421db9cc5759641aff2702d6
shubham <shubhamrewamp17@gmail.com>
2025-10-23 23:49:57 +0530

    some work

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,5 +1,5 @@
 import express  from "express"  ;
-import { addCourse, addThumbnail, removePhotoCloudinary } from "../controller/CourseController.js";
+import { addCourse, addThumbnail, editCourse, getCourseById, getCreaterCourse, removeCourseById, removePhotoCloudinary } from "../controller/CourseController.js";
 import isAuth from "../middleware/isAuth.js";
 
 const CourseRouter = express.Router() ; 
@@ -9,6 +9,10 @@ const CourseRouter = express.Router() ;
 CourseRouter.post("/add-course" , isAuth  ,  addCourse)
 CourseRouter.post("/add-thumbnail" , isAuth  , addThumbnail  )
 CourseRouter.post("/cloudinary/delete"  , removePhotoCloudinary)
+CourseRouter.post("/edit-course" , isAuth  , editCourse) 
+CourseRouter.delete("/delete-course/:courseId" , isAuth , removeCourseById ) 
+CourseRouter.get("/creater-course", isAuth ,  getCreaterCourse )
+CourseRouter.get("/getcourse-by-id/:couresId" , isAuth , getCourseById)
 
 
 
